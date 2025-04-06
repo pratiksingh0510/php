5d4db5781d8b6a3c13ffd7ccab7c406200f0630d
Pratik Kumar Singh <31792153+pratiksingh05@users.noreply.github.com>
2025-04-06 14:47:00 +0530

    added dockerfile for php on render app

--- googleads.php
+++ googleads.php
@@ -1,20 +1,9 @@
 <?php
 
 // Enable CORS
-$allowed_origins = [
-    "https://www.marvellousmilestone.com",
-    "https://marvellousmilestone.com",
-	"http://localhost:5173/"
-];
-
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-}
-
+header("Access-Control-Allow-Origin: *"); // For dev, allow all. Replace '*' with your Vue app URL in prod.
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Content-Type: application/json");
-header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type");
 
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
